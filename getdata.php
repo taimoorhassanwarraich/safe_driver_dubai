3043f937d9de80815fe4e37e50db43f88a993fea
Taimoor hassan <taimoorwarch@gmail.com>
2023-09-19 21:52:37 +0500

    initial comment

---
+++ getdata.php
@@ -0,0 +1,10 @@
+<php>
+    $name=$_Post['name'];
+    $email=$_Post['email'];
+    $phone=$_Post['phone'];
+    $date=$_Post['date'];
+    $time=$_Post['time'];
+    $drop=$_Post['drop'];
+    $pickup=$_Post['pickup'];
+    echo '<script>alert($name)</script>'
+</php>
---
+++ index.js
@@ -0,0 +1,176 @@
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+  navToggler.classList.toggle("active");
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("open");
+  if (nav.classList.contains("open")) {
+    nav.style.maxHeight = nav.scrollHeight + "px";
+  } else {
+    nav.removeAttribute("style");
+  }
+}
+let index = 0;
+// vars
+("use strict");
+var testim = document.getElementById("testim"),
+  testimDots = Array.prototype.slice.call(
+    document.getElementById("testim-dots").children
+  ),
+  testimContent = Array.prototype.slice.call(
+    document.getElementById("testim-content").children
+  ),
+  testimLeftArrow = document.getElementById("left-arrow"),
+  testimRightArrow = document.getElementById("right-arrow"),
+  testimSpeed = 4500,
+  currentSlide = 0,
+  currentActive = 0,
+  testimTimer,
+  touchStartPos,
+  touchEndPos,
+  touchPosDiff,
+  ignoreTouch = 30;
+window.onload = function () {
+  // Testim Script
+  function playSlide(slide) {
+    for (var k = 0; k < testimDots.length; k++) {
+      testimContent[k].classList.remove("active");
+      testimContent[k].classList.remove("inactive");
+      testimDots[k].classList.remove("active");
+    }
+
+    if (slide < 0) {
+      slide = currentSlide = testimContent.length - 1;
+    }
+
+    if (slide > testimContent.length - 1) {
+      slide = currentSlide = 0;
+    }
+
+    if (currentActive != currentSlide) {
+      testimContent[currentActive].classList.add("inactive");
+    }
+    testimContent[slide].classList.add("active");
+    testimDots[slide].classList.add("active");
+
+    currentActive = currentSlide;
+
+    clearTimeout(testimTimer);
+    testimTimer = setTimeout(function () {
+      playSlide((currentSlide += 1));
+    }, testimSpeed);
+  }
+
+  testimLeftArrow.addEventListener("click", function () {
+    playSlide((currentSlide -= 1));
+  });
+
+  testimRightArrow.addEventListener("click", function () {
+    playSlide((currentSlide += 1));
+  });
+
+  for (var l = 0; l < testimDots.length; l++) {
+    testimDots[l].addEventListener("click", function () {
+      playSlide((currentSlide = testimDots.indexOf(this)));
+    });
+  }
+
+  playSlide(currentSlide);
+
+  // keyboard shortcuts
+  document.addEventListener("keyup", function (e) {
+    switch (e.keyCode) {
+      case 37:
+        testimLeftArrow.click();
+        break;
+
+      case 39:
+        testimRightArrow.click();
+        break;
+
+      case 39:
+        testimRightArrow.click();
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  testim.addEventListener("touchstart", function (e) {
+    touchStartPos = e.changedTouches[0].clientX;
+  });
+
+  testim.addEventListener("touchend", function (e) {
+    touchEndPos = e.changedTouches[0].clientX;
+
+    touchPosDiff = touchStartPos - touchEndPos;
+
+    console.log(touchPosDiff);
+    console.log(touchStartPos);
+    console.log(touchEndPos);
+
+    if (touchPosDiff > 0 + ignoreTouch) {
+      testimLeftArrow.click();
+    } else if (touchPosDiff < 0 - ignoreTouch) {
+      testimRightArrow.click();
+    } else {
+      return;
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const bookingForm = document.getElementById("bookingForm");
+
+  bookingForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Gather form data
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const drop = document.getElementById("drop").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+    const phone = document.getElementById("phone").value;
+    const pickup = document.getElementById("pickup").value;
+    // Gather other form data as needed
+
+    // Create an object with the form data
+    const formData = {
+      name: name,
+      email: email,
+      phone: phone,
+      drop: drop,
+      pickup: pickup,
+      time: time,
+      date: date,
+      // Add other form fields here
+    };
+
+    // Make an API call using Fetch
+    // Make an API call using Fetch
+    fetch("http://localhost:3000/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.text(); // Read the response as text
+      })
+      .then((data) => {
+        console.log("API response:", data);
+        alert("Booking request sent successfully!");
+      })
+      .catch((error) => {
+        console.error("API error:", error);
+        alert("Error: Something went wrong while sending the booking request.");
+      });
+  });
+});
